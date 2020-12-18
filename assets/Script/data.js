43720f63ec1a998fa11015e396bee16a11e473e6
DESKTOP-O05QI8S\YAN <tthuyen2808@gmail.com>
2020-12-18 15:03:25 +0700

    game vẽ hình

--- assets/Script/data.js
+++ assets/Script/data.js
@@ -1,56 +1,139 @@
+
+// Quy ước: Đặt thứ tự của các điểm là trái trước. Nếu cùng cấp thì trên trước
 module.exports = {
   levelData: [
-  {   arrayPain:
-    [
-      new cc.Vec2(100, 100),
-      new cc.Vec2(100, 480),
-      new cc.Vec2(510, 480),
-     // new cc.Vec2(510, 100),
-    ],
-    destsArray:
-    [
-      [
-        { end: 1, isDraw: false },
-        { end: 2, isDraw: false },
+
+   /* {
+      arrayPain: [new cc.Vec2(100, 150)],
+      destsArray: [
+        [{ end: 1, isDraw: false },{ end: 2, isDraw: false },],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false },],
+        [{ end: 0, isDraw: false },{ end: 1, isDraw: false },],
       ],
-      [
-        { end: 0, isDraw: false },
-        { end: 2, isDraw: false },
+    }, */
+
+
+
+    {
+      arrayPain: [new cc.Vec2(100, 100), new cc.Vec2(100, 480), new cc.Vec2(510, 480)],
+      destsArray: [
+        [{ end: 1, isDraw: false}/*,{ end: 2, isDraw: false } */],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false },],
+        [{ end: 0, isDraw: false, oneWay: true },{ end: 1, isDraw: false },],
       ],
-     [
-        { end: 0, isDraw: false },
-        { end: 1, isDraw: false },
-      ]
-     ]
     },
 
-    {   arrayPain:
-      [
-        new cc.Vec2(100, 100),
-        new cc.Vec2(100, 480),
-        new cc.Vec2(510, 480),
-        new cc.Vec2(510, 100),
+    {
+      arrayPain: [ new cc.Vec2(100, 100),new cc.Vec2(100, 480),new cc.Vec2(510, 480),new cc.Vec2(510, 100),
       ],
-      destsArray:
-      [
-        [
-          { end: 1, isDraw: false },
-          { end: 3, isDraw: false },
-        ],
-        [
-          { end: 0, isDraw: false },
-          { end: 2, isDraw: false },
-        ],
-       [
-          { end: 1, isDraw: false },
-          { end: 3, isDraw: false },
-        ],
-        [
-          { end: 0, isDraw: false },
-          { end: 2, isDraw: false },
-        ]
-       ]
-      }
-    
-    ]
+      destsArray: [
+        [{ end: 1, isDraw: false }, { end: 3, isDraw: false },],
+        [{ end: 0, isDraw: false }, { end: 2, isDraw: false }],
+        [{ end: 1, isDraw: false }, { end: 3, isDraw: false }],
+        [{ end: 0, isDraw: false }, { end: 2, isDraw: false }],
+      ],
+    },
+    {
+      arrayPain: [ new cc.Vec2(320, 530),new cc.Vec2(80, 340),new cc.Vec2(530, 340),new cc.Vec2(440, 100),new cc.Vec2(150, 100), ],
+      destsArray: [
+        [{ end: 3, isDraw: false }, { end: 4, isDraw: false }],
+        [{ end: 2, isDraw: false },{ end: 3, isDraw: false }],
+        [{ end: 1, isDraw: false },{ end: 4, isDraw: false },],
+        [{ end: 1, isDraw: false },{ end: 0, isDraw: false },],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false }],
+      ],
+    },
+    {
+      arrayPain: [new cc.Vec2(100, 350), new cc.Vec2(320, 500), new cc.Vec2(320, 350),new cc.Vec2(320, 180), new cc.Vec2(540, 350) ],
+      destsArray: [
+        [ { end: 1, isDraw: false },{ end: 2, isDraw: false }],
+        [{ end: 0, isDraw: false },  { end: 2, isDraw: false }, { end: 4, isDraw: false } ],
+        [{ end: 0, isDraw: false },  { end: 1, isDraw: false }, { end: 3, isDraw: false },{ end: 4, isDraw: false } ],
+        [{ end: 2, isDraw: false }],
+        [{ end: 1, isDraw: false }, { end: 2, isDraw: false }],
+      ],
+    }, 
+    {
+      arrayPain: [new cc.Vec2(300, 520), new cc.Vec2(100, 380), new cc.Vec2(480, 380), new cc.Vec2(100, 120), new cc.Vec2(480, 120) ],
+      destsArray: [
+        [ { end: 1, isDraw: false }, { end: 2, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false },{ end: 3, isDraw: false } ],
+        [{ end: 1, isDraw: false }, { end: 0, isDraw: false }, { end: 4, isDraw: false } ],
+        [{ end: 1, isDraw: false },{ end: 4, isDraw: false }],
+        [{ end: 2, isDraw: false },{ end: 3, isDraw: false } ],
+      ],
+    },
+
+    { 
+      arrayPain: [new cc.Vec2(150, 100),new cc.Vec2(150, 480), new cc.Vec2(450, 480),new cc.Vec2(450, 100),new cc.Vec2(450, 340),new cc.Vec2(300, 340),new cc.Vec2(300, 480) ],
+      destsArray: [
+        [ { end: 1, isDraw: false },{ end: 3, isDraw: false },{ end: 4, isDraw: false }],
+        [ { end: 0, isDraw: false }, { end: 6, isDraw: false }],
+       [ { end: 4, isDraw: false }, { end: 6, isDraw: false } ],
+       [{ end: 4, isDraw: false }, { end: 0, isDraw: false } ],
+       [{ end: 5, isDraw: false }, { end: 2, isDraw: false }, { end: 0, isDraw: false },{ end: 3, isDraw: false } ],
+       [{ end: 6, isDraw: false }, { end: 4, isDraw: false } ],
+       [ { end: 1, isDraw: false }, { end: 2, isDraw: false }, { end: 5, isDraw: false }]],
+    }, 
+
+    {
+      arrayPain: [new cc.Vec2(100, 500), new cc.Vec2(100, 100), new cc.Vec2(300, 400), new cc.Vec2(500, 500), new cc.Vec2(500, 100)],
+      destsArray: [
+        [{ end: 1, isDraw: false },{ end: 2, isDraw: false },],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false },{ end: 4, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 1, isDraw: false }, { end: 3, isDraw: false },{ end: 4, isDraw: false }],
+        [{ end: 2, isDraw: false },{ end: 4, isDraw: false }],
+        [{ end: 1, isDraw: false },{ end: 2, isDraw: false }, { end: 3, isDraw: false }],
+      ],
+    },
+
+    {
+      arrayPain: [new cc.Vec2(100, 350), new cc.Vec2(150, 100), new cc.Vec2(300, 500), new cc.Vec2(300, 250), new cc.Vec2(500, 350), new cc.Vec2(450, 100) ],
+      destsArray: [
+        [{ end: 1, isDraw: false },{ end: 2, isDraw: false }, { end: 3, isDraw: false },  { end: 4, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 5, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 3, isDraw: false }, { end: 4, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false }, { end: 4, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false }, { end: 3, isDraw: false }, { end: 5, isDraw: false }],
+        [{ end: 1, isDraw: false },{ end: 4, isDraw: false }],
+      ],
+    },
+    {
+      arrayPain: [new cc.Vec2(120, 420), new cc.Vec2(180, 100), new cc.Vec2(300, 500), new cc.Vec2(500, 420), new cc.Vec2(400, 100)],
+      destsArray: [
+        [{ end: 1, isDraw: false },{ end: 2, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false }, { end: 3, isDraw: false }, { end: 4, isDraw: false }],
+        [{ end: 0, isDraw: false }, { end: 1, isDraw: false }, { end: 3, isDraw: false }, { end: 4, isDraw: false }],
+        [{ end: 2, isDraw: false },{ end: 1, isDraw: false },{ end: 4, isDraw: false }],
+        [{ end: 1, isDraw: false }, { end: 2, isDraw: false }, { end: 3, isDraw: false }],
+      ],
+    },
+  
+    {
+      arrayPain: [new cc.Vec2(100, 550), new cc.Vec2(100, 350), new cc.Vec2(100, 50), new cc.Vec2(300, 350), new cc.Vec2(500, 550), new cc.Vec2(500, 350), new cc.Vec2(500, 50)],
+      destsArray: [
+        [{ end: 1, isDraw: false },{ end: 3, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false }, { end: 3, isDraw: false }],
+        [{ end: 1, isDraw: false }, { end: 6, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 1, isDraw: false },{ end: 4, isDraw: false },{ end: 5, isDraw: false }],
+        [{ end: 3, isDraw: false }, { end: 5, isDraw: false }],
+        [{ end: 3, isDraw: false }, { end: 4, isDraw: false }, { end: 6, isDraw: false }],
+        [{ end: 2, isDraw: false }, { end: 5, isDraw: false }],
+      ],
+    },
+    {
+      arrayPain: [new cc.Vec2(100, 350), new cc.Vec2(100, 200), new cc.Vec2(100, 50), new cc.Vec2(300, 500), new cc.Vec2(500, 350), new cc.Vec2(500, 200), new cc.Vec2(500, 50)],
+      destsArray: [
+        [{ end: 1, isDraw: false },{ end: 3, isDraw: false }, { end: 4, isDraw: false },{ end: 5, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 2, isDraw: false }, { end: 5, isDraw: false }],
+        [{ end: 1, isDraw: false },{ end: 6, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 4, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 3, isDraw: false }, { end: 5, isDraw: false }],
+        [{ end: 0, isDraw: false },{ end: 1, isDraw: false }, { end: 4, isDraw: false }, { end: 6, isDraw: false }],
+        [{ end: 2, isDraw: false },{ end: 5, isDraw: false }],
+
+      ],
+    },
+
+  ],
 };
--- assets/Script/draw.js
+++ assets/Script/draw.js
@@ -36,14 +36,17 @@ cc.Class({
     win: false,
     index: -1,
     level: 0,
+    playAgainButton: cc.Button,
+    arrowSprite: cc.Node,
   },
 
   // LIFE-CYCLE CALLBACKS:
 
   onLoad() {
+    console.log("Hello");
     this.overNode.active = false;
     this.level = 0;
-    this.newLevel();
+    this.newGame();
       var self = this;
   this.drawNode.on(cc.Node.EventType.TOUCH_START, function (event) {
       if (self.win == true) return;
@@ -101,6 +104,10 @@ cc.Class({
             }
           }
           self.index = self.destsArray[self.index][i].end;
+          if(self.checkGameOver() == true) {
+            console.log("checkGameOver");
+            self.playAgainButton.node.active = true;
+          }
           hadFound = true;
           if (self.checkWin()) {
             console.log("...........win");
@@ -115,23 +122,44 @@ cc.Class({
     }
     });
     this.drawNode.on(cc.Node.EventType.TOUCH_END, function (event) {
+      var daVe = false;
+    for(var i = 0; i < self.destsArray.length; i++) {
+      for (var j = 0; j < self.destsArray[i].length; j++) {
+        if(self.destsArray[i][j].isDraw == true) {
+          daVe = true;
+          break;
+        } 
+      }
+      if(daVe)  break;
+    }
+    if(daVe == false) {
+      self.index = -1;
+    }
     self.clearLine();
     }); 
   },
-  newLevel() {
+  newGame() {
     console.log("level: " + this.level);
-
     this.clearAll();
     this.arrayPain = data.levelData[this.level].arrayPain;
     this.destsArray = data.levelData[this.level].destsArray;
+    for(var i = 0; i < this.destsArray.length; i++) {
+      for (var j = 0; j < this.destsArray[i].length; j++) {
+        this.destsArray[i][j].isDraw = false;
+      }
+    }
     this.drawWall();
     this.drawHideLine();
     this.drawCycles();
+    this.index = -1;
+    this.playAgainButton.node.active = false;
+    this.overNode.active = false;
+
   },
   drawWall() {
     var g = this.boardNode.getComponent(cc.Graphics);
     g.lineWidth = 10;
-    g.strokeColor.fromHEX("#ff0000");
+    g.strokeColor.fromHEX("#000000");
     g.rect(
       leftPadding / 2,
       leftPadding / 2,
@@ -144,7 +172,7 @@ cc.Class({
   drawCycles() {
     var g = this.boardNode.getComponent(cc.Graphics);
     g.lineWidth = 1;
-    g.fillColor.fromHEX("#ff0000");
+    g.fillColor.fromHEX("#006600");
     if(this.arrayPain != null) {
       for (var i = 0; i < this.arrayPain.length; i++) {
         g.circle(this.arrayPain[i].x, this.arrayPain[i].y, bankinh);
@@ -160,7 +188,7 @@ cc.Class({
     var g = this.drawNode.getComponent(cc.Graphics);
     g.clear();
     g.lineWidth = 10;
-    g.strokeColor.fromHEX("#ff0000");
+    g.strokeColor.fromHEX("#CCFFCC");
     var position = this.arrayPain[this.index];
     g.moveTo(position.x, position.y);
     g.lineTo(vec2.x, vec2.y);
@@ -169,7 +197,7 @@ cc.Class({
   drawLineNew(p1, p2) {
     var g = this.boardNode.getComponent(cc.Graphics);
     g.lineWidth = 10;
-    g.strokeColor.fromHEX("#ff0000");
+    g.strokeColor.fromHEX("#336600");
     g.moveTo(p1.x, p1.y);
     g.lineTo(p2.x, p2.y);
     g.stroke();
@@ -182,6 +210,18 @@ cc.Class({
       for (var j = 0; j < this.destsArray[i].length; j++) {
         var begin = this.arrayPain[i];
         var end = this.arrayPain[this.destsArray[i][j].end];
+        console.log("vitri: " + i + "..." + this.destsArray[i][j].end  + "...."+ begin.x + "," + begin.y + "; " + end.x + "," + end.y );
+        if( this.destsArray[i][j].oneWay) {
+          console.log("oneway: " + i + "..." + this.destsArray[i][j].end  + "...."+ this.destsArray[i][j].oneWay);
+          var vitriX = (Math.abs(begin.x + end.x))/2;
+          var vitriY = (Math.abs(begin.y + end.y))/2;
+          console.log("diem: " + vitriX + "," + vitriY);
+          var arrowNode = cc.instantiate(this.arrowSprite);
+          arrowNode.position = new cc.Vec2(vitriX, vitriY);
+          this.boardNode.addChild(arrowNode);
+          var angle = this.tinhGoc(begin, end);
+          arrowNode.angle = angle;
+        } 
         g.moveTo(begin.x, begin.y);
         g.lineTo(end.x, end.y);
         g.stroke();
@@ -225,25 +265,52 @@ cc.Class({
   },
   
   resetGame() {
-    if(this.level <= this.arrayPain.length) {
+    if(this.level < data.levelData.length) {
       this.level++;
       console.log("level: " + this.level);
-      this.newLevel();
-    } else {
-      console.log("endgame");
-    }
+      this.newGame();
+    } 
+    },
 
-  },
   clearLine() {
     var g = this.drawNode.getComponent(cc.Graphics);
     g.clear();
   },
   clearAll() {
+    this.boardNode.removeAllChildren();
     var g = this.drawNode.getComponent(cc.Graphics);
     g.clear();
     var graphics = this.boardNode.getComponent(cc.Graphics);
     graphics.clear();
   },
+  checkGameOver() {
+    if(this.index == -1) return;
+    if (this.checkWin() == true) return;
+    var end = true;
+    for (var i = 0; i < this.destsArray[this.index].length; i++) {
+        if (this.destsArray[this.index][i].isDraw == false) {
+          end = false;
+        }
+    }
+    return end;
+  },
+  playAgainGame() {
+    this.newGame();
+  },
+  tinhGoc(start, end) {
+    var deltaX = end.x - start.x;
+    var deltaY = end.y - start.y;
+    goc1 = Math.atan(deltaY/deltaX);
+    goc1 = (goc1 * 180)/Math.PI;
+    if(deltaX < 0) {
+      if(deltaY > 0) {
+        goc1 = 180 - goc1;
+      } else {
+        goc1 = 180 + goc1;
+      }
+    }
+    return goc1;
+  }
   //start () {},
-  update (dt) { },
+  //update (dt) { },
 });
