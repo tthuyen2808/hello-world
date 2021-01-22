0abd9ee2e3ba5ec134e0b33db00cb30e774a74e6
DESKTOP-G66T6LH\YAN <tthuyen2808@gmail.com>
2021-01-22 10:30:09 +0700

    Vẽ đường ngang, dọc game caro

--- assets/Script/carotTest.js
+++ assets/Script/carotTest.js
@@ -6,8 +6,8 @@
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 const leftPadding = 20;
 const boardSize = 600;
-const cellPading = 1;
-const soHang = 20;
+const cellPading = 0;
+const soHang = 10;
 const doRong = boardSize / soHang;
 cc.Class({
     extends: cc.Component,
@@ -28,7 +28,7 @@ cc.Class({
         //         this._bar = value;
         //     }
         // },
-        caroArray:[],
+        caroArray: [],
         drawNode: cc.Node,
         user: -1,
         labelX: cc.Node,
@@ -37,43 +37,51 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-     onLoad () {
-        this.drawItem(20);
+    onLoad() {
+        this.drawItem(soHang);
         this.caroArray = [
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],
-            [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0]
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ];
         var self = this;
         this.drawNode.on(cc.Node.EventType.TOUCH_START, function (event) {
             var x = event.getLocationX();
             var y = event.getLocationY();
             var position = self.drawNode.convertToNodeSpaceAR(cc.v2(x, y));
-            var deltaX = Math.round(position.x - (position.x % doRong) + doRong/2);
+            console.log(position.x + "," + position.y + ".." + doRong);
+            var hang = Math.floor(position.x / doRong);
+            var cot = Math.floor(position.y / doRong);
+            var midPointX = doRong * hang + 1 / 2 * doRong;
+            var midPointY = doRong * cot + 1 / 2 * doRong;
+            console.log(midPointX + "-" + midPointY);
+            self.drawCycle(midPointX,midPointY);
+            /*var deltaX = Math.round(position.x - (position.x % doRong) + doRong/2);
             var deltaY = Math.round(position.y - (position.y % doRong) + doRong/2);
             var vitri = self.tinhHangCot(deltaX,deltaY);
+            console.log("hang: " + vitri.hang + "- cot: " + vitri.cot);
             if(self.user % 2 == 0) {
                 self.caroArray[vitri.hang][vitri.cot] = "1";
             } else {
                 self.caroArray[vitri.hang][vitri.cot] = "2";
-            }
+            }*/
         });
         this.drawNode.on(cc.Node.EventType.TOUCH_END, function (event) {
             self.user = self.user + 1;
@@ -81,37 +89,59 @@ cc.Class({
     },
 
 
-     drawItem(soHang) {
+    drawItem(soHang) {
         var g = this.drawNode.getComponent(cc.Graphics);
-        g.lineWidth = 1;
-        g.strokeColor.fromHEX('#000000');
-        for (var hang = 0; hang < soHang; hang++) {
-            for (var cot = 0; cot < soHang ; cot++) {
-                g.roundRect(leftPadding + hang * doRong, leftPadding + cellPading + doRong * cot, doRong - cellPading * 2, doRong - cellPading * 2, 0);
-            }
+        g.lineWidth = 4;
+        g.strokeColor.fromHEX('#ff0000');
+        /*      for (var hang = 0; hang < soHang; hang++) {
+                  for (var cot = 0; cot < soHang ; cot++) {
+                      g.roundRect(hang * doRong, doRong * cot, doRong, doRong, 0);
+                  }
+              }
+              g.fill();
+              for (var x = 0; x < soHang; x++) {
+                  for (var y = 0; y < soHang; y++) {
+                      g.strokeColor.fromHEX('#000000');
+                      g.roundRect(leftPadding + cellPading + x * doRong, leftPadding + cellPading + doRong * y, doRong - cellPading * 2, doRong - cellPading * 2, 0);
+                      g.stroke();
+                  }
+              }   */
+        g.roundRect(0, 0, boardSize, boardSize, 0);
+        console.log("drawItem");
+        //g.stroke();
+        //Vẽ đường line ngang
+        for (var i = 0; i < soHang; i++) {
+            g.moveTo(0, i * doRong);
+            g.lineTo(boardSize, i * doRong);
         }
+        // Vẽ đường dọc
+        for (var i = 0; i < soHang; i++) {
+            g.moveTo(i * doRong, 0);
+            g.lineTo(i * doRong, boardSize);
+        }
+        g.stroke();
+        //
+    },
+    drawCycle(x,y) {
+        var g = this.drawNode.getComponent(cc.Graphics);
+        g.lineWidth = 4;
+        g.fillColor.fromHEX('#ff0000');
+        g.circle(x,y,10);
         g.fill();
-        for (var x = 0; x < soHang; x++) {
-            for (var y = 0; y < soHang; y++) {
-                g.strokeColor.fromHEX('#000000');
-                g.roundRect(leftPadding + cellPading + x * doRong, leftPadding + cellPading + doRong * y, doRong - cellPading * 2, doRong - cellPading * 2, 0);
-                g.stroke();
-            }
-        }   
-},
+    },
 
-addLabel(x,y) {
-   
-    if(this.user % 2 == 0) {
-        var labelX = cc.instantiate(this.labelX);
-        labelX.parent = this.drawNode;
-        labelX.setPosition(x, y);
-    } else {
-        var labelO = cc.instantiate(this.labelO);
-        labelO.parent = this.drawNode;
-        labelO.setPosition(x, y);
-    }
-},
+    addLabel(x, y) {
+
+        if (this.user % 2 == 0) {
+            var labelX = cc.instantiate(this.labelX);
+            labelX.parent = this.drawNode;
+            labelX.setPosition(x, y);
+        } else {
+            var labelO = cc.instantiate(this.labelO);
+            labelO.parent = this.drawNode;
+            labelO.setPosition(x, y);
+        }
+    },
 
     tinhHangCot(x, y) {
         var cot = Math.floor((x - leftPadding) / doRong);
@@ -123,41 +153,41 @@ addLabel(x,y) {
         return vitri;
     },
 
-     check5Square(startRow, startColumn, value) {
+    check5Square(startRow, startColumn, value) {
         var count = 0;
         // check hàng Chéo từ trên xuống
         for (var i = 0; i < 5; i++) {
             // o = (i,i) -> Ex: o = (2,2), (3,3), (4,4)
-            if(this.caroArray[startRow + i][startColumn + i] == value) {
+            if (this.caroArray[startRow + i][startColumn + i] == value) {
                 count++;
             }
             if (count == 5) {
+                var result = new Object();
+                result.type = "CheoXuong";
+                result.startRow = startRow;
+                result.startColumn = startColumn;
+                return result;
+            }
+        }
+
+        // check hàng Chéo từ dưới lên
+        count = 0;
+        for (var i = 0; i < 5; i++) {
+            if (this.caroArray[startRow + 4 - i][startColumn + i] == value) {
+                count++;
+            }
+        }
+        if (count == 5) {
             var result = new Object();
-            result.type = "CheoXuong";
+            result.type = "CheoLen";
             result.startRow = startRow;
             result.startColumn = startColumn;
             return result;
         }
-        }
-       
-       // check hàng Chéo từ dưới lên
-         count = 0;
-         for (var i = 0; i < 5; i++) {
-             if (this.caroArray[startRow + 4 - i][startColumn + i] == value) {
-                 count++;
-             }
-         }
-         if (count == 5) {
-             var result = new Object();
-             result.type = "CheoLen";
-             result.startRow = startRow;
-             result.startColumn = startColumn;
-             return result;
-         }
 
-         // check hàng Ngang
-        
-         for (var i = startRow; i < startRow + 5; i++) {
+        // check hàng Ngang
+
+        for (var i = startRow; i < startRow + 5; i++) {
             count = 0; //reset lại biến đếm sau mỗi dòng for
             for (var j = startColumn; j < startColumn + 5; j++) {
                 if (this.caroArray[i][j] == value) {
@@ -174,7 +204,7 @@ addLabel(x,y) {
             }
         }
         // check hàng Dọc
-       for (var i = startColumn; i < startColumn + 5; i++) {
+        for (var i = startColumn; i < startColumn + 5; i++) {
             count = 0; //reset lại biến đếm sau mỗi dòng for
             for (var j = startRow; j < startRow + 5; j++) {
                 if (this.caroArray[j][i] == value) {
@@ -188,31 +218,31 @@ addLabel(x,y) {
                 result.startColumn = i;
                 return result;
             }
-        } 
+        }
         return null;
     },
     checkAll() {
         var len = this.caroArray.length;
-        for(var i = 0; i <= len - 5 ; i++) {
-            for(var j = 0; j <= len - 5; j++){
-                var result1 = this.check5Square(i,j,1);
-                if(result1 != null) {
+        for (var i = 0; i <= len - 5; i++) {
+            for (var j = 0; j <= len - 5; j++) {
+                var result1 = this.check5Square(i, j, 1);
+                if (result1 != null) {
                     console.log("1 win");
-                   return;
-                } else {
-                    var result2 = this.check5Square(i,j,2);
-                    if(result2 != null) {
-                        console.log("2 win");
                     return;
+                } else {
+                    var result2 = this.check5Square(i, j, 2);
+                    if (result2 != null) {
+                        console.log("2 win");
+                        return;
                     }
                 }
-               
+
             }
         }
         console.log("chua win");
     }
 
-  //  start () {},
+    //  start () {},
 
     // update (dt) {},
 });
